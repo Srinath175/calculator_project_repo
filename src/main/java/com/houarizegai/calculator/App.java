259779f66433d7d0ad224c090f3443f61f85895e
Srinath175 <srinath.jella@skywavessoftware.com>
2024-11-23 11:19:34 +0530

    Update App.java

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -2,7 +2,7 @@ package com.houarizegai.calculator;
 
 import com.houarizegai.calculator.ui.CalculatorUI;
 
-public class App extends SpringServletContainerInitializer {
+public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         new CalculatorUI();
